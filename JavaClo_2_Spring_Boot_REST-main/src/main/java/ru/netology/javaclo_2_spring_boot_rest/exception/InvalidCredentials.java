54757d6e48434400080a6ca123fdbc42fc278097
Дмитрий Циркин <102895852+Dimka5555@users.noreply.github.com>
2022-12-04 13:11:05 +0300

    Delete JavaClo_2_Spring_Boot_REST-main directory

--- JavaClo_2_Spring_Boot_REST-main/src/main/java/ru/netology/javaclo_2_spring_boot_rest/exception/InvalidCredentials.java
+++
@@ -1,7 +0,0 @@
-package ru.netology.javaclo_2_spring_boot_rest.exception;
-
-public class InvalidCredentials extends RuntimeException {
-    public InvalidCredentials(String msg) {
-        super(msg);
-    }
-}
